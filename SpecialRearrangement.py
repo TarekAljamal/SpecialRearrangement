f7581a4bdccacc4f5ebe959ca4c8a934ae7c68c4
Tarek Aljamal <tarekaljamal@outlook.com>
2024-11-04 06:12:42 +0200

    A Python code that takes a list of integers and rearranges it so that all even numbers appear before all odd numbers, while preserving the original relative order of both even and odd numbers.

---
+++ SpecialRearrangement.py
@@ -0,0 +1,17 @@
+
+# A Python function that takes a list of integers as a parameter and rearranges it so that even numbers
+# in this list are before odd numbers
+
+def special_rearrangement(numbers):
+    rearranged_list = []
+    for i in range(len(numbers)):
+        if((numbers[i] % 2) == 0):
+            rearranged_list.append(numbers[i])
+            
+    for i in range(len(numbers)):
+        if((numbers[i] % 2) != 0):
+            rearranged_list.append(numbers[i])    
+
+    numbers = rearranged_list
+    print(numbers)        
+    return numbers
